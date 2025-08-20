4edc13537254ca8dbe4e023e88688548c375b7f4
BarcosRigoberto <gustavobar2006@gmail.com>
2025-08-20 15:52:19 -0300

    Agregado archivos principales de SeñApp

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php   
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido a SeñApp</h1>
+    <button><a href="Login.php">Iniciar sesion</a></button> <button><a href="Registro.php">Registrarse</a></button>
+</body>
+</html>
