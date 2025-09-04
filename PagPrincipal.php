af579b2a06639f83be9a0233213e66e55fa1c413
BarcosRigoberto <gustavobar2006@gmail.com>
2025-09-04 16:01:34 -0300

    Cambios 4/9
    Se esta trabajando en la implementacion de niveles

--- PagPrincipal.php
+++ PagPrincipal.php
@@ -7,7 +7,7 @@ $result = $conexion->query("SELECT id FROM nivel ORDER BY id ASC");
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>SeñApp - Mapa de Niveles</title>
     
 </head>
@@ -16,13 +16,13 @@ $result = $conexion->query("SELECT id FROM nivel ORDER BY id ASC");
         <h1>SeñApp</h1>
         <h2>Aprende Lenguaje de Señas</h2>
     </header>
-    <h2>Mapa de Niveles</h2>
     <div class="niveles-lista">
         <?php while($row = $result->fetch_assoc()): ?>
-            <button class="nivel-btn" href="nivel.php?id=<?php echo $row['id']; ?>"> </button> 
-            <a class="nivel-link" href="nivel.php?id=<?php echo $row['id']; ?>">
-             <?php echo $row['id']; ?>  
-            </a>
+            <div class="nivel-btn">
+                <a class="nivel-link" href="nivel.php?id=<?php echo $row['id']; ?>">
+                    <?php echo $row['id']; ?>
+                </a>
+            </div>
         <?php endwhile; ?>
     </div>
 </body>
