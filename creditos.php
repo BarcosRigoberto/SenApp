e9b92eda2e322caade770ae337363e64cb1dd7a2
BarcosRigoberto <gustavobar2006@gmail.com>
2025-11-10 21:30:02 -0300

    Add files via upload

---
+++ creditos.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creditos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<style>
+    .datos{
+        margin-right: 20px;
+        padding: 20px;
+        text-align:center;
+    }
+</style>
+<body>
+    <header>
+        <h1>Creditos</h1>
+    </header>
+    <div class="datos">
+    
+    <h3>Emilio Appella - emilioappella@gmail.com</h3>
+    <h3>Gustavo Barcos - gustavobar2006@gmail.com</h3>
+    <h3>Agustin Ardenghi - </h3>
+    <h3>C.P. B7000 Tandil, Buenos Aires, Argentina</h3>
+    <h3>Grupo GEBA</h3>
+    <button><a href="PagPrincipal.php">Volver</a></button>
+    </div>
+</body>
+</html>
---
+++ premium.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Premium</title>
+</head>
+<body>
+    <div class="gratis">
+        <h1>Plan Gratuito</h1>
+        Anuncios <br>
+        <Button>
+            <a href="PagPrincipal.php">
+            Continuar Plan Gratuito
+            </a>
+        </Button>
+    </div>
+    <div class="premium">
+        <h1>Plan SeñApp+</h1>
+        Sin interrupciones. <br>
+        Prueba gratis de 7 dias. <br>
+        
+       
+        <button><h2>Mensual: $1.500 ARS</h2></button>
+        <button> <h2>Anual: $15.000 ARS</h2></button>
+    </div>
+</body>
+</html>
